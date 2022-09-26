4dd3a8fd710b9ab399d57752baf7e3793f02b8e0
Radhaiya <kulkarnir27999@gmail.com>
2022-09-26 21:02:58 +0530

    Day 20

---
+++ Find similarities between two arrays..cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+pair < int , int > findSimilarity(vector < int > arr1, vector < int > arr2, int n, int m) 
+{
+	int count=0;   
+    
+    for(int i=0;i<arr1.size();i++){
+        for(int j=0;j<arr2.size();j++){
+            if(arr1[i]==arr2[j]){
+                count++;
+            }
+        }
+    }
+    
+    return make_pair(count,n+m-count);
+}
